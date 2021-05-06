0b077dbb1ff1ba29324d8a3499ea456debfdf2e2
RayyanNafees <nafees.rayyan@gmail.com>
2021-05-06 22:30:12 +0530

    Made a material Ui Form

---
+++ src/Signup.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Form, InputGroup, FormControl } from "react-bootstrap";
+import "./login.css";
+
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { indigo } from "@material-ui/core/colors";
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText(indigo[500]),
+    backgroundColor: indigo[500],
+    "&:hover": {
+      backgroundColor: indigo[700],
+    },
+  },
+}))(Button);
+
+function Input({ tab, ph }) {
+  return (
+    <Form.Group>
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1">{tab}</InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          placeholder={ph}
+          aria-label={ph}
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+    </Form.Group>
+  );
+}
+
+export default function SignUp() {
+  return (
+    <Form className="login">
+      <h1>Sign Up</h1>
+      <Input tab="@" ph="Email" />
+      <Input tab="@" ph="Password" />
+      <Input tab="@" ph="Password Confirmation" />
+      <ColorButton
+        variant="contained"
+        color="primary"
+        style={{ width: "100%" }}
+      >
+        Submit
+      </ColorButton>
+      <p style={{marginTop:15}}>
+        By clicking this button, you agree to our
+        <br />
+        <a href="/privacy">Privacy Policy</a> &{" "}
+        <a href="/terms">Terms of Use</a>
+      </p>
+    </Form>
+  );
+}
